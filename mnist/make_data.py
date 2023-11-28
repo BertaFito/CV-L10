36d0289a5d7912da919e43fc380ab499a20ffe77
BertaFito <96293450+BertaFito@users.noreply.github.com>
2023-11-28 18:52:45 +0100

    Prepare data

--- mnist/make_data.py
+++ mnist/make_data.py
@@ -17,8 +17,8 @@ import random
 from zipfile import ZipFile 
 
 SIZE = 416
-images_num_train = 500
-images_num_test = 50
+images_num_train = 500    # 1000
+images_num_test = 50      # 200
 
 image_sizes = [3, 6, 3] # small, medium, big
 
